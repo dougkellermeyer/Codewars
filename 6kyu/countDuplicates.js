766297168fd569422e3c413d127e05dface24f88
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-05-01 17:20:26 -0400

    more regex

--- 6kyu/countDuplicates.js
+++ 6kyu/countDuplicates.js
@@ -1,5 +1,8 @@
 function duplicateCount(text){
-    return text
+    if (text.length > 0) {
+        var duplicates = text.toLowerCase().split("").sort().join("").match(/(.)\1+/g)
+        return (duplicates === null ? 0 : duplicates.length);
+    } return 0
 }
 
-console.log(duplicateCount("yeet"))
+console.log(duplicateCount("aabc"))
