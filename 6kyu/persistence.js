d1975aaf4de409e669d080ecff27daadfcf557e4
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-05-01 15:24:55 -0400

    solved persistence

--- 6kyu/persistence.js
+++ 6kyu/persistence.js
@@ -6,36 +6,18 @@
 //  persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
                             // and 4 has only one digit
 
-function persistence(num){
-    //check to see if the num is >=1
-    if (num >=1 && num <= 9){
-        return 0;
-    } else {
-    
-        var numStr = num.toString()
-        var numArray = [] //[1,2]
-
-        for(let i = 0; i < numStr.length; i++){
-            numArray.push(+numStr.charAt(i));
-        }
-
-        var reduceArray = numArray.reduce((acc, curVal) => {
-            return acc * curVal
-        })
-
-        tmp = reduceArray.toString()
-        console.log(tmp.length)
-        if (tmp.length === 1){
-            console.log("equal to 1")
-        }else {
-            console.log("more than 1 digit")
-            var numArray2 = [] 
-
-            for(let j = 0; j < tmp.length; j++){
-                numArray2.push(+tmp.charAt(j));
-            }
-        }
+function persistence(num) {
+    //check to see if num is 2 digits, if not return zero
+    if(num < 9 ) return 0;
+    //feed num into for loop
+    for (var i = 0; num > 9; i++) {
+        //make num a string, then an array of the 'digits'
+        //now reduce the array, if num is still 2+ digits (>9), loop again
+        num = num.toString().split('').reduce((a, b) => b * a);
     }
+    //return the number of iterations it took to get the num product to one digit
+    return i;
 }
 
-persistence(99) //1
+console.log(persistence(8)) //4 (9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, 1*2 = 2)
+
