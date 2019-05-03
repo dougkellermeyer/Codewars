e7a86b77a0c2df2faae8a9580cb57fe29b5e0193
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-05-03 09:00:17 -0400

    refactored to String instead of toString()

--- 6kyu/countDuplicates.js
+++ 6kyu/countDuplicates.js
@@ -2,7 +2,7 @@ function duplicateCount(text){
     if (text.length > 0) {
         var duplicates = text.toLowerCase().split("").sort().join("").match(/(.)\1+/g)
         return (duplicates === null ? 0 : duplicates.length);
-    } return 0
+    } return 0;
 }
 
-console.log(duplicateCount("aabc"))
+console.log(duplicateCount(""))
--- 6kyu/persistence.js
+++ 6kyu/persistence.js
@@ -6,18 +6,18 @@
 //  persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
                             // and 4 has only one digit
 
-function persistence(num) {
+const persistence = (num) => {
     //check to see if num is 2 digits, if not return zero
     if(num < 9 ) return 0;
     //feed num into for loop
     for (var i = 0; num > 9; i++) {
         //make num a string, then an array of the 'digits'
         //now reduce the array, if num is still 2+ digits (>9), loop again
-        num = num.toString().split('').reduce((a, b) => b * a);
+        num = String(num).split('').reduce((a, b) => b * a,1);
     }
     //return the number of iterations it took to get the num product to one digit
     return i;
 }
 
-console.log(persistence(8)) //4 (9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, 1*2 = 2)
+console.log(persistence(999)) //4 (9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, 1*2 = 2)
 
