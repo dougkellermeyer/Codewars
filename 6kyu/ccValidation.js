01265279a528ac9a19cf691a7698f1f3a8a02a43
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-05-09 16:51:34 -0400

    another one

--- 6kyu/ccValidation.js
+++ 6kyu/ccValidation.js
@@ -1,5 +1,17 @@
-function validate(n){
+// In this Kata, you will implement the Luhn Algorithm, which is used to help validate credit card numbers.
 
+// Given a positive integer of up to 16 digits, return true if it is a valid credit card number, and false if it is not.
+
+// Here is the algorithm:
+
+// 1. Double every other digit, scanning from right to left, starting from the second digit (from the right).
+// 2. If a resulting number is greater than 9, replace it with the sum of its own digits 
+//  (which is the same as subtracting 9 from it)
+// 3. Sum all the final digits (reduce would work well here)
+// 4. Divide the sum by 10, if the remainder = 0 return true, if not then return false
+
+function validate(n){
+    return n.map(x => x*2)
 }
 
-validate([1,2,3,4])
+console.log(validate([1,2,3,4]))
