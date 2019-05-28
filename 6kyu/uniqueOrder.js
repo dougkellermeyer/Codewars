ec1baeba98775550884e63360212e73124f650b8
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-05-28 10:38:37 -0400

    another one

--- 6kyu/uniqueOrder.js
+++ 6kyu/uniqueOrder.js
@@ -2,6 +2,8 @@ function uniqueInOrder (list) {
     const arr = Array.isArray(list) ? list : list.split('')
   
     return arr.reduce((acc, v) => {
+      //compare the first index of acc (acc[i]) to each letter
+      //if they are equal, ignore as repeated, if not equal push to acc
       if (acc[acc.length - 1] !== v) {
         acc.push(v)
       }
