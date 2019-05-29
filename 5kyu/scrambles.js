8b6c75b66b3973e38217200feaaff296d490614f
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-05-29 15:48:39 -0400

    need to refactor

--- 5kyu/scrambles.js
+++ 5kyu/scrambles.js
@@ -2,33 +2,17 @@
 //portion of str1 characters can be rearranged to match str2, 
 //otherwise returns false.
 
-function scramble(str1,str2){
-
-    // function removeDuplicates(){
-        //make this a funciton to clean strings/array
-    // }
-    var removeStr2Duplicates = str2
-    .split('')
-    .filter(function(item, pos, self) {
-    return self.indexOf(item) == pos;
-    })
-    .join('');
-    var matchArr = [];
-    for(let i = 0; i < str1.length; i++){
-        for(let j = 0; j < str2.length; j++){
-            if(str1[i] === str2[j]){
-                matchArr.push(str1[i])
-            }
+function scramble(str1, str2) {
+    var array1 = str1.split("").sort();
+    var array2 = str2.split("").sort();
+    var i = 0;
+    for(var x = 0; i < array2.length && x <= array1.length; x++) {
+        if(array2[i] === array1[x]) {
+            i++;
         }
     }
-    var removeDuplicates =  matchArr.join('')
-                    .split('')
-                    .filter(function(item, pos, self) {
-                    return self.indexOf(item) == pos;
-                    })
-                    .join('');
-
-    return removeDuplicates.split('').sort().join('') === removeStr2Duplicates.split('').sort().join('') ? true : false
+    return (x <= array1.length);
 }
 
-console.log(scramble('scriptingjava','javascript'))
+console.log(scramble('cedewaraaossoqqyt', 'codewars'))
+
