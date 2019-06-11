8dcd3571e8c524d46e9aeefd083a578a69bef9a7
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-06-11 16:15:17 -0400

    started a new problem

---
+++ 7kyu/arrayReverser.js
@@ -0,0 +1,13 @@
+//Given an array of strings, reverse them and their order in such way that their length stays the same as the length of the original inputs.
+
+//Ex. 
+//Input:  {"I", "like", "big", "butts", "and", "I", "cannot", "lie!"}
+//Output: {"!", "eilt", "onn", "acIdn", "ast", "t", "ubgibe", "kilI"}
+
+const ultimateReverse = s => {
+    //return the array but with the items reversed
+    let arrReverse = s.reverse() //[ 'lie!', 'cannot', 'I', 'and', 'butts', 'big', 'like', 'I' ]
+    
+}
+
+console.log(ultimateReverse(["I", "like", "big", "butts", "and", "I", "cannot", "lie!"]))
