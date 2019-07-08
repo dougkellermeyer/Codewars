710f358ad9528e91fae097441c4f4202f2eb6ed2
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2019-07-07 22:22:12 -0400

    took long enough

--- 8kyu/countBy.js
+++ 8kyu/countBy.js
@@ -1,5 +1,18 @@
+//Create a function with two arguments that will return a list of length (n) with multiples of (x).
+
+//Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+//Return the results as an array
+
 function countBy(x, n) {
     var z = [];
+
+    for(let i = 1; i <= n; i++){
+        z.push(i*x)
+    }
   
     return z;
-  }
+}
+
+console.log(countBy(2,5))
+
