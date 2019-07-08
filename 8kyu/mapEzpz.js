64ebae225e0dcb20c3030d8451d05093492048d0
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-07-08 11:19:55 -0400

    ezpz

--- 8kyu/mapEzpz.js
+++ 8kyu/mapEzpz.js
@@ -1,8 +1,7 @@
 //Given an array of integers, return a new array with each value doubled.
 
 function maps(arr){
-    
-
+    return arr.map(x => x * 2)
 }
 
-maps([1,2,3])
+console.log(maps([1,2,3]))
