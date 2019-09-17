29f0d67cf6a820595cf3734a7a14741c586cd512
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-09-17 16:10:20 -0400

    refactor:

--- 8kyu/sumMixedArray.js
+++ 8kyu/sumMixedArray.js
@@ -1,16 +1,18 @@
 arr = [9,2, '1','2']
 
-function sumMix(a){
-    var stringNumsArray = [];
-    for(i=0; i < a.length; i++){
-        stringNumsArray.push(parseInt(a[i],10))
-    }
-    return stringNumsArray.reduce((acc,curr) => acc+curr);
-}
+// function sumMix(a){
+//     var stringNumsArray = [];
+//     for(i=0; i < a.length; i++){
+//         stringNumsArray.push(parseInt(a[i],10))
+//     }
+//     return stringNumsArray.reduce((acc,curr) => acc+curr);
+// }
 
-sumMix(arr);
+
 
 //refactor using reduce instead of loop
-// function sumMix(x){
-//     return x.reduce((s,x)=>s+ +x,0);
-// }
+function sumMix(a){
+    return a.reduce((acc,curr) => acc+ +curr, 0);
+}
+
+console.log(sumMix(arr));
