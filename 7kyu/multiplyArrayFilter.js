ee9241625416f35b330b9f90c0b2dfe553ef4cf0
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-10-01 15:54:47 -0400

    setup for map, do it to it

---
+++ 7kyu/multiplyArrayFilter.js
@@ -0,0 +1,8 @@
+var numArray = [1, 2, 3, 4];
+
+function multiplyAndFilter(arr, multi){
+    console.log(arr, multi);
+    //map the array with the multiplier
+};
+
+multiplyAndFilter(numArray, 1.5);
