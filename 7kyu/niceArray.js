5fecdb74d5cf2c4fdfcd7cdbad24f14d501041ea
Kellermeyer,Doug <dkellermeyer#paychex.com>
2019-10-01 13:54:35 -0400

    some changes

--- 7kyu/niceArray.js
+++ 7kyu/niceArray.js
@@ -1,23 +1,22 @@
-const arr1 = [2,10,9,3];
+const arr1 = [3,4,5,7];
 
 function isNice(arr){
+    //count the number of matches, should be equal to the length of the array becuase each number(n) must be equal to n+1 || n-1
+    var counter = 0;
     for(let i = 0; i < arr.length; i++){
         //take each element in the array, see if adding or subtracting 1 is equal to another index in the array
-        for(let j = 0; j < i; j++){
-            console.log((Math.abs(arr[i] - arr[j])))
+        if(arr.includes(arr[i] - 1) || arr.includes(arr[i] + 1)){
+            counter++
+        }else{
+            return false;
         }
     };
+    //compare our counter to the arr.length. Check to see if array is empty
+    if(counter === arr.length && arr.length != 0){
+        return true
+    }else {
+        return false
+    }
 };
 
 console.log(isNice(arr1));
-
-//something like this...
-
-// function isNice(arr){
-//     var counter = 0
-//     for(var i = 0; i < arr.length; i++) {
-//         if (arr.includes(arr[i] - 1) || arr.includes(arr[i] + 1)) counter ++
-//     }
-//     if (counter == arr.length && arr.length != 0) return true
-//     return false
-// }
--- 8kyu/sumMixedArray.js
+++ 8kyu/sumMixedArray.js
@@ -1,16 +1,6 @@
 arr = [9,2, '1','2']
 
-// function sumMix(a){
-//     var stringNumsArray = [];
-//     for(i=0; i < a.length; i++){
-//         stringNumsArray.push(parseInt(a[i],10))
-//     }
-//     return stringNumsArray.reduce((acc,curr) => acc+curr);
-// }
-
-
-
-//refactor using reduce instead of loop
+//use unary plus operator in reduce to convert strings to numbers
 function sumMix(a){
     return a.reduce((acc,curr) => acc+ +curr, 0);
 }
