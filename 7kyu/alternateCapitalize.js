d6117ff87fbe1fd32cfea952a7924add35637c34
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2020-07-30 13:54:43 -0400

    more problems

---
+++ 7kyu/alternateCapitalize.js
@@ -0,0 +1,9 @@
+function capitalize(s){
+    const even = s.split("").map((l, i) => i % 2 === 0 ? l.toUpperCase() : l).join("");
+    const odd = s.split("").map((l, i) => i % 2 !== 0 ? l.toUpperCase() : l).join("");
+    
+    return [even, odd];
+  };
+
+console.log(capitalize("abcdef"))
+
---
+++ 7kyu/arrayMultiplier.js
@@ -0,0 +1,18 @@
+// In this Kata, you will be given a multi-dimensional array containing 2 or more sub-arrays of integers. Your task is to find the maximum product that can be formed by taking any one element from each sub-array.
+
+// Examples:
+// solve( [[1, 2],[3, 4]] ) = 8. The max product is given by 2 * 4
+// solve( [[10,-15],[-1,-3]] ) = 45, given by (-15) * (-3)
+// solve( [[1,-1],[2,3],[10,-100]] ) = 300, given by (-1) * 3 * (-100)
+
+//get the largest number in each array
+
+const getMaxProduct = (arr) => {
+    let products = [];
+    for(let i = 0; i < arr.length; i++){
+        products.push(arr[i].map(Math.abs).sort().slice(-1))
+    }
+    return products.reduce((a,b) => a*b);
+};
+
+console.log(getMaxProduct([ [-1,2,-3,4],[1,-2,3,-4] ]));
